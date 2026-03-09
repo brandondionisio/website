93130c36b7d3a280613a6a4a16415bf544efb60e
brandondionisio <brandon.dionisio@differenthealth.com>
2026-03-09 15:31:12 -0400

    avatar randomized on client-side and removed tailwind screen config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,10 +3,6 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			screens: {
-				// Viewport width at which cutoff: styles apply (e.g. desktop layout)
-				'cutoff': '78px',
-			},
 			colors: {
 				primary: 'var(--text-primary)',
 				secondary: 'var(--text-secondary)',
